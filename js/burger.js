8081fe78aa4801f5055c8ba289434810637cb412
Nikita <125561376+Nikitaa-Rudenko@users.noreply.github.com>
2024-05-19 17:27:29 +0300

    Add files via upload

---
+++ js/burger.js
@@ -0,0 +1,24 @@
+const BURGER_OPENED_CLASSNAME = 'burger_open';
+const BURGER_BTN_OPENED_CLASSNAME = 'burger-btn_open';
+const BODY_FIXED_CLASSNAME_SECOND = 'body_fixed';
+
+const bodyNode_SECOND = document.querySelector('body');
+const burgerNode = document.querySelector('.js-burger');
+const burgerBtnNode = document.querySelector('.js-burger-btn');
+const burgerContentNode = document.querySelector('.js-burger__content')
+
+burgerBtnNode.addEventListener('click', toggleBurger);
+
+burgerNode.addEventListener('click', (event) => {
+    const isClickOutsideContent = !event.composedPath().includes(burgerContentNode)
+
+    if (isClickOutsideContent) {
+        toggleBurger();
+    }
+})
+
+function toggleBurger() {
+    burgerNode.classList.toggle(BURGER_OPENED_CLASSNAME);
+    bodyNode_SECOND.classList.toggle(BODY_FIXED_CLASSNAME_SECOND);
+    burgerBtnNode.classList.toggle(BURGER_BTN_OPENED_CLASSNAME);
+}
